2a8a9e3828b732b913ac6bd9555bd8bbbb2d7b11
JaimeSag <langsamlernen@gmail.com>
2025-11-30 15:48:25 -0600

    Enhance Image component to handle loading errors and apply conditional styling

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,19 +1,24 @@
-import { ImgHTMLAttributes } from "react";
+import { ImgHTMLAttributes, useState } from "react";
+import { cn } from "../lib/utils";
 
 export function Image({
   src,
   loading = "lazy",
   decoding = "auto",
+  className,
   ...props
 }: ImgHTMLAttributes<HTMLImageElement>) {
+  const [error, setError] = useState(false);
   const imageSrc = src || "/assets/img/default.jpg";
 
   return (
     <img
+      className={cn((error || !src) && "opacity-30 dark:opacity-10", className)}
       src={imageSrc}
       onError={(e) => {
         e.currentTarget.onerror = null;
         e.currentTarget.src = "/assets/img/default.jpg";
+        setError(true);
       }}
       loading={loading}
       decoding={decoding}
